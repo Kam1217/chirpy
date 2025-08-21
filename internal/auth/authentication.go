4534986e68ccd04c77a2db385b742d4b14ed42bd
Kam1217 <kamilaostroga@gmail.com>
2025-08-21 16:42:37 +0100

    hash password function

--- internal/auth/authentication.go
+++ internal/auth/authentication.go
@@ -1,8 +1,16 @@
 package auth
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) (string, error) {
-	bcrypt.GenerateFromPassword()
-	return "", nil
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("failed to hash password: %w", err)
+	}
+
+	return string(hash), nil
 }
