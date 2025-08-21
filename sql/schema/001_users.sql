24a297e80dd4cf4f94c2c74c897cfa8aebb80e21
Kam1217 <kamilaostroga@gmail.com>
2025-08-21 11:20:42 +0100

    email

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
     ID UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    eamil TEXT NOT NULL UNIQUE
+    email TEXT NOT NULL UNIQUE
 );
 
 -- +goose Down
